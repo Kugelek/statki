875e323e8eee222ec7ec0ebf79c8c5bd6b0b2ceb
swks14 <majk@1337.hub.pl>
2020-11-18 18:02:39 +0100

    dodano rejestracje i logowanie

---
+++ statki/src/main/java/com/projekt/statki/service/UserService.java
@@ -0,0 +1,12 @@
+package com.projekt.statki.service;
+
+import com.projekt.statki.model.User;
+import com.projekt.statki.web.dto.UserRegistrationDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
+
+
+public interface UserService extends UserDetailsService{
+	User save(UserRegistrationDto registrationDto);
+}
