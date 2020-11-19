094b6f7942627ed7391ee101d91eafd06d248409
swks14 <majk@1337.hub.pl>
2020-11-19 23:42:25 +0100

    XD czemu sie nie dodalo

---
+++ statki/src/main/java/com/projekt/statki/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.projekt.statki.repository;
+
+import com.projekt.statki.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+	User findByEmail(String email);
+}
