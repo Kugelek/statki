b99d2af8b9ec0ed58720cd6bce4fc824414c8ac0
Piotr Kaiser <piotrkaiser@trenerok.pl>
2021-05-07 19:36:10 +0200

    latest pull

--- statki/src/main/java/com/projekt/statki/web/dto/UserRegistrationDto.java
+++ statki/src/main/java/com/projekt/statki/web/dto/UserRegistrationDto.java
@@ -4,17 +4,17 @@ public class UserRegistrationDto {
 	private String firstName;
 	private String lastName;
 	private String email;
+	private String nick;
 	private String password;
 	
-	public UserRegistrationDto(){
-		
-	}
+	public UserRegistrationDto() {}
 	
-	public UserRegistrationDto(String firstName, String lastName, String email, String password) {
+	public UserRegistrationDto(String firstName, String lastName, String email, String nick, String password) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
+		this.nick = nick;
 		this.password = password;
 	}
 	
@@ -35,6 +35,12 @@ public class UserRegistrationDto {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
 	}
 	public String getPassword() {
 		return password;
